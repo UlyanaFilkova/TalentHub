68350811949fa841c375eeaa3f2f056cc1a7ca03
ulyanafilkova <ulyanafilkova56@gmail.com>
2025-01-21 13:34:04 +0300

    feat: change nuxt config and remove scss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import { resolve } from 'path';
 
 export default defineNuxtConfig({
 	compatibilityDate: '2024-11-01',
 	devtools: { enabled: false },
 	modules: ['@nuxt/eslint'],
-	alias: {
-		'@': resolve(__dirname, '/'),
-	},
-	css: ['~/assets/styles/main.scss'],
+
+	css: ['~/assets/styles/main.css'],
 	postcss: {
 		plugins: {
 			tailwindcss: {},
