0e9a9f40f8b43ae82459af1ca2b93885b0c04dbd
Egor <sea25042003@gmail.com>
2025-01-23 19:13:52 +0300

    started working on tabs on user profile page

---
+++ layouts/userProfile.vue
@@ -0,0 +1,54 @@
+<template>
+	<NuxtLayout name="default">
+		<template #default>
+			<nav class="flex gap-4">
+				<NuxtLink
+					:to="`/users/${id}/profile`"
+					:class="[
+						'p-2 text-white no-underline',
+						isActive('/profile')
+							? 'border-b-2 border-solid border-red-800 text-red-800'
+							: 'text-white',
+					]"
+				>
+					Profile
+				</NuxtLink>
+				<NuxtLink
+					:to="`/users/${id}/skills`"
+					:class="[
+						'p-2 text-white no-underline',
+						isActive('/skills')
+							? 'border-b-2 border-solid border-red-800 text-red-800'
+							: 'text-white',
+					]"
+				>
+					Skills
+				</NuxtLink>
+				<NuxtLink
+					:to="`/users/${id}/languages`"
+					:class="[
+						'p-2 text-white no-underline',
+						isActive('/languages')
+							? 'border-b-2 border-solid border-red-800 text-red-800'
+							: 'text-white',
+					]"
+				>
+					Languages
+				</NuxtLink>
+			</nav>
+
+			<div class="mt-4">
+				<slot />
+			</div>
+		</template>
+	</NuxtLayout>
+</template>
+
+<script setup>
+	import { useRoute } from 'vue-router';
+
+	const route = useRoute();
+	const id = route.params.id;
+
+	const isActive = (tab) => route.path.endsWith(tab);
+</script>
--- pages/users/[id]/profile.vue
+++ pages/users/[id]/profile.vue
@@ -6,7 +6,7 @@
 		</div>
 		<div class="flex flex-col items-center justify-center">
 			<h5 class="mb-2 text-2xl text-white">abba</h5>
-			<p class="text-upload-color">{{ user?.email }}</p>
+			<!-- <p class="text-upload-color">{{ user?.email }}</p> -->
 			<p class="text-white">A member since 20.12.2024</p>
 		</div>
 		<form>form</form>
@@ -14,22 +14,25 @@
 </template>
 
 <script setup lang="ts">
-	import { getUserById } from '~/composables/services/user/user-service';
+	// import { getUserById } from '~/composables/services/user/user-service';
 
+	definePageMeta({
+		layout: 'user-profile',
+	});
 	const route = useRoute();
 	const userId = ref(route.params.id);
 	console.log('id', userId.value);
-	const { user, loading, error } = getUserById(userId.value as string);
-	watch(
-		() => route.params.id,
-		(newUserId) => {
-			if (newUserId !== userId.value) {
-				userId.value = newUserId;
-				const { user: newUser } = getUserById(userId.value as string);
-				user.value = newUser.value;
-			}
-		}
-	);
+	// const { user, loading, error } = getUserById(userId.value as string);
+	// watch(
+	// 	() => route.params.id,
+	// 	(newUserId) => {
+	// 		if (newUserId !== userId.value) {
+	// 			userId.value = newUserId;
+	// 			const { user: newUser } = getUserById(userId.value as string);
+	// 			user.value = newUser.value;
+	// 		}
+	// 	}
+	// );
 </script>
 
 <style scoped></style>
