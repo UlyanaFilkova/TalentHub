13420c80c0272d6e64aa0d7124b6d20db876e3b0
Egor <sea25042003@gmail.com>
2025-01-25 01:23:55 +0300

    added somewhat working BaseButton component

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,50 @@ export default {
 				'inactive-color': 'var(--color-inactive-text)',
 				'second-color': 'var(--color-second-text)',
 				'accent-color': 'var(--color-accent-color)',
+
+				button: {
+					primary: {
+						contained: {
+							background: 'var(--color-button-primary-contained-background)',
+							text: 'var(--color-button-primary-contained-text)',
+							hover: 'var(--color-button-primary-contained-hover)',
+						},
+						outlined: {
+							border:
+								'rgb(var(--color-button-primary-outlined-border) / <alpha-value>)',
+							borderHover: 'var(--color-button-primary-outlined-border-hover)',
+							text: 'var(--color-button-primary-outlined-text)',
+							background:
+								'rgb(var(--color-button-primary-outlined-background) / <alpha-value>)',
+						},
+						text: {
+							text: 'var(--color-button-primary-text-text)',
+							background:
+								'rgb(var(--color-button-primary-text-background) / <alpha-value>)',
+						},
+					},
+					secondary: {
+						contained: {
+							background: 'var(--color-button-secondary-contained-background)',
+							text: 'var(--color-button-secondary-contained-text)',
+							hover: 'var(--color-button-secondary-contained-hover)',
+						},
+						outlined: {
+							border:
+								'rgb(var(--color-button-secondary-outlined-border) / <alpha-value>)',
+							borderHover:
+								'var(--color-button-secondary-outlined-border-hover)',
+							text: 'var(--color-button-secondary-outlined-text)',
+							background:
+								'rgb(var(--color-button-secondary-outlined-background) / <alpha-value>)',
+						},
+						text: {
+							text: 'var(--color-button-secondary-text-text)',
+							background:
+								'rgb(var(--color-button-secondary-text-background) / <alpha-value>)',
+						},
+					},
+				},
 			},
 			fontFamily: {
 				sans: ['Roboto', 'ui-sans-serif', 'system-ui'],
