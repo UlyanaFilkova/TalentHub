f89c1fea5f4a27b1593b8baa0df24cccddf9e5ac
Egor <sea25042003@gmail.com>
2025-01-28 14:46:25 +0300

    still in the process of implementing user profile with dynamic data fetching and update functionality

--- pages/users/[id]/profile.vue
+++ pages/users/[id]/profile.vue
@@ -5,35 +5,32 @@
 			<FileUpload />
 		</div>
 		<div class="flex flex-col items-center justify-center">
-			<h5 class="font- font-me mb-2 text-2xl font-light text-white">abba</h5>
-			<p class="text-upload-color">eshkere@gmail.com</p>
-			<p class="text-white">A member since 20.12.2024</p>
+			<h5 class="font-me mb-2 text-2xl font-light text-white">
+				{{ fullName }}
+			</h5>
+			<p class="text-upload-color">{{ email }}</p>
+			<p class="text-white">A member since {{ dateJoined }}</p>
 		</div>
-		<form class="grid w-full max-w-3xl grid-cols-2 gap-x-8 gap-y-9">
-			<BaseInput
-				id="firstName"
-				label="First Name"
-				@input-value="updateFirstName"
-			/>
-			<BaseInput
-				id="lastName"
-				label="Last Name"
-				@input-value="updateLastName"
-			/>
+		<form
+			class="grid w-full max-w-3xl grid-cols-1 gap-x-8 gap-y-9 px-4 md:grid-cols-2 md:pr-12"
+			@submit.prevent="handleSubmit"
+		>
+			<BaseInput id="firstName" v-model="firstName" label="First Name" />
+			<BaseInput id="lastName" v-model="lastName" label="Last Name" />
 
 			<BaseDropdown
 				id="department"
+				v-model="selectedDepartment"
 				label="Department"
 				:options="departments"
-				default-option-label="Select Department"
-				@select-value="updateDepartment"
+				default-option-label="No department"
 			/>
 			<BaseDropdown
 				id="position"
+				v-model="selectedPosition"
 				label="Position"
 				:options="positions"
-				default-option-label="Select Position"
-				@select-value="updatePosition"
+				default-option-label="No position"
 			/>
 
 			<div></div>
@@ -50,25 +47,142 @@
 </template>
 
 <script setup lang="ts">
-	// import { getUserById } from '~/composables/services/user/user-service';
+	import {
+		getAllDepartments,
+		getAllPositions,
+		getUserById,
+		updateProfile,
+		updateUser,
+	} from '~/services/user/user-service';
+
+	interface Option {
+		value: string;
+		label: string;
+	}
 
 	definePageMeta({
 		layout: 'user-profile',
 	});
+
 	const route = useRoute();
 	const userId = ref(route.params.id);
-	console.log('id', userId.value);
-	// const { user, loading, error } = getUserById(userId.value as string);
-	// watch(
-	// 	() => route.params.id,
-	// 	(newUserId) => {
-	// 		if (newUserId !== userId.value) {
-	// 			userId.value = newUserId;
-	// 			const { user: newUser } = getUserById(userId.value as string);
-	// 			user.value = newUser.value;
-	// 		}
-	// 	}
-	// );
+
+	const firstName = ref('');
+	const lastName = ref('');
+	const fullName = ref('');
+	const email = ref('');
+	const dateJoined = ref('');
+	const selectedDepartment = ref<Option>({ value: '', label: '' });
+	const selectedPosition = ref<Option>({ value: '', label: '' });
+
+	const departments = ref<Option[]>([]);
+	const positions = ref<Option[]>([]);
+
+	const isSubmitting = ref(false);
+
+	onMounted(() => {
+		const {
+			user: fetchedUser,
+			loading,
+			error,
+		} = getUserById(userId.value as string);
+
+		const { departments: fetchedDepartments } = getAllDepartments();
+		const { positions: fetchedPositions } = getAllPositions();
+
+		watch(
+			fetchedDepartments,
+			(newDepartments) => {
+				if (newDepartments) {
+					departments.value = newDepartments.map((dept) => ({
+						value: dept.id,
+						label: dept.name,
+					}));
+				}
+			},
+			{ immediate: true }
+		);
+
+		watch(
+			fetchedPositions,
+			(newPositions) => {
+				if (newPositions) {
+					positions.value = newPositions.map((pos) => ({
+						value: pos.id,
+						label: pos.name,
+					}));
+				}
+			},
+			{ immediate: true }
+		);
+
+		watch(
+			fetchedUser,
+			(newUser) => {
+				if (newUser) {
+					firstName.value = newUser.profile.first_name || '';
+					lastName.value = newUser.profile.last_name || '';
+					fullName.value = newUser.profile.full_name || '';
+					email.value = newUser.email || '';
+					dateJoined.value = newUser.created_at
+						? new Date(parseInt(newUser.created_at)).toDateString()
+						: '';
+					selectedDepartment.value.value = newUser.department?.id || '';
+					selectedDepartment.value.label = newUser.department?.name || '';
+
+					selectedPosition.value.value = newUser.position?.id || '';
+					selectedPosition.value.label = newUser.position?.name || '';
+				}
+			},
+			{ immediate: true }
+		);
+
+		watch(
+			() => route.params.id,
+			(newUserId) => {
+				if (newUserId !== userId.value) {
+					userId.value = newUserId;
+					const { user: newUser } = getUserById(userId.value as string);
+					fetchedUser.value = newUser.value;
+				}
+			}
+		);
+	});
+	const handleSubmit = async () => {
+		isSubmitting.value = true;
+		try {
+			const { executeUpdate: updateUserData } = updateUser({
+				userId: userId.value as string,
+				departmentId: selectedDepartment.value.value,
+				positionId: selectedPosition.value.value,
+			});
+			const { executeUpdate: updateProfileData } = updateProfile({
+				userId: userId.value as string,
+				first_name: firstName.value,
+				last_name: lastName.value,
+			});
+			const [userResult, profileResult] = await Promise.all([
+				updateUserData(),
+				updateProfileData(),
+			]);
+			const { refetch } = getUserById(userId.value as string);
+			const result = await refetch();
+			if (result?.data?.user) {
+				firstName.value = result.data.user.profile.first_name || '';
+				lastName.value = result.data.user.profile.last_name || '';
+				fullName.value = result.data.user.profile.full_name || '';
+				selectedDepartment.value.value = result.data.user.department?.id || '';
+				selectedDepartment.value.label =
+					result.data.user.department?.name || '';
+				selectedPosition.value.value = result.data.user.position?.id || '';
+				selectedPosition.value.label = result.data.user.position?.name || '';
+			}
+		} catch (error) {
+			console.error('Error submitting form:', error);
+		} finally {
+			isSubmitting.value = false;
+		}
+	};
 </script>
 
 <style scoped></style>
---
+++ services/user/user-mutations.ts
@@ -0,0 +1,72 @@
+import { gql } from '@apollo/client/core';
+
+export const GET_USER_BY_ID = gql`
+	query User($userId: ID!) {
+		user(userId: $userId) {
+			id
+			created_at
+			email
+			is_verified
+			profile {
+				id
+				first_name
+				last_name
+				full_name
+				avatar
+			}
+			department {
+				id
+				name
+			}
+			position {
+				id
+				name
+			}
+			role
+		}
+	}
+`;
+
+export const UPDATE_USER = gql`
+	mutation UpdateUser($user: UpdateUserInput!) {
+		updateUser(user: $user) {
+			id
+			department {
+				id
+				name
+			}
+			position {
+				id
+				name
+			}
+		}
+	}
+`;
+
+export const UPDATE_PROFILE = gql`
+	mutation UpdateProfile($profile: UpdateProfileInput!) {
+		updateProfile(profile: $profile) {
+			id
+			first_name
+			last_name
+		}
+	}
+`;
+
+export const GET_ALL_DEPARTMENTS = gql`
+	query Departments {
+		departments {
+			id
+			name
+		}
+	}
+`;
+
+export const GET_ALL_POSITIONS = gql`
+	query Positions {
+		positions {
+			id
+			name
+		}
+	}
+`;
