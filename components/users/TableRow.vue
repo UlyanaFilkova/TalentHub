869d23ad9e03adf89720f4aec2f27108efc0db88
ulyanafilkova <ulyanafilkova56@gmail.com>
2025-01-28 14:08:28 +0300

    fix: center user profile button in table row

--- components/users/TableRow.vue
+++ components/users/TableRow.vue
@@ -1,34 +1,30 @@
 <template>
 	<tr class="border-b border-low-contrast-color text-sm text-active-color">
-		<td class="px-2">
+		<td class="px-2 py-4">
 			<BaseUserPic
 				:name="`${row.firstName} ${row.lastName}`"
 				class="bg-profilepic-color"
 				:photo="row.photo"
 			/>
 		</td>
-		<td class="px-3 py-2 font-medium">
+		<td class="px-3 py-4 font-medium">
 			{{ row.firstName }}
 		</td>
-		<td class="px-4 py-2 font-medium">
+		<td class="px-4 py-4 font-medium">
 			{{ row.lastName }}
 		</td>
-		<td class="px-4 py-2">
+		<td class="px-4 py-4">
 			{{ row.email }}
 		</td>
-		<td class="px-4 py-2">
+		<td class="px-4 py-4">
 			{{ row.department }}
 		</td>
-		<td class="px-4 py-2">
+		<td class="px-4 py-4">
 			{{ row.position }}
 		</td>
-		<td class="px-4 py-2">
+		<td class="px-4 py-4">
 			<NuxtLink :to="`/users/${row.id}/profile`">
-				<ButtonsToggle
-					is-toggled
-					class="mb-4 ml-2"
-					color="var(--color-inactive-text)"
-				/>
+				<ButtonsToggle is-toggled color="var(--color-inactive-text)" />
 			</NuxtLink>
 		</td>
 	</tr>
