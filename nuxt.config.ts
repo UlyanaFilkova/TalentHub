d49aa16afa0709bc0c08981a34495b157efbba4f
Yana <yana.prokopenko24@mai.ru>
2025-01-30 20:21:16 +0100

    merge commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 
 export default defineNuxtConfig({
 	compatibilityDate: '2024-11-01',
-	devtools: { enabled: true },
+	devtools: { enabled: false },
 	modules: ['@nuxt/eslint'],
 	css: ['~/assets/styles/main.css'],
 	plugins: ['~/plugins/apolloClient'],
