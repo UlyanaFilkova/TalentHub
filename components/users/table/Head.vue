ccb89d45850f61ad3b290676ea97651d6577244d
ulyanafilkova <ulyanafilkova56@gmail.com>
2025-01-30 20:33:37 +0300

    add TableHead

---
+++ components/users/table/Head.vue
@@ -0,0 +1,35 @@
+<template>
+	<thead class="sticky top-0 z-20 bg-background">
+		<tr>
+			<th
+				v-for="header in headers"
+				:key="header.key"
+				class="border-b border-low-contrast-color px-4 pb-4 pt-1 text-left text-sm font-medium text-active-color"
+			>
+				<div class="flex items-center gap-2">
+					<span>{{ header.label }}</span>
+					<button
+						v-if="header.isSortable"
+						class="flex w-6 justify-center text-low-contrast-color"
+						@click="sortTable(header.key)"
+					>
+						<template v-if="sortKey === header.key">
+							<span v-if="sortOrder === 'asc'">▲</span>
+							<span v-else>▼</span>
+						</template>
+						<template v-else>▲▼</template>
+					</button>
+				</div>
+			</th>
+		</tr>
+	</thead>
+</template>
+
+<script setup lang="ts">
+	defineProps<{
+		headers: { key: string; label: string; isSortable: boolean }[];
+		sortKey: string | null;
+		sortOrder: 'asc' | 'desc';
+		sortTable: (key: string) => void;
+	}>();
+</script>
--- components/users/table/index.vue
+++ components/users/table/index.vue
@@ -1,30 +1,12 @@
 <template>
 	<div class="max-h-full w-full">
 		<table class="w-full table-auto overflow-x-auto">
-			<thead class="sticky top-0 z-20 bg-background">
-				<tr>
-					<th
-						v-for="header in headers"
-						:key="header.key"
-						class="border-b border-low-contrast-color px-4 pb-4 pt-1 text-left text-sm font-medium text-active-color"
-					>
-						<div class="flex items-center gap-2">
-							<span>{{ header.label }}</span>
-							<button
-								v-if="header.isSortable"
-								class="flex w-6 justify-center text-low-contrast-color"
-								@click="sortTable(header.key)"
-							>
-								<template v-if="sortKey === header.key">
-									<span v-if="sortOrder === 'asc'">▲</span>
-									<span v-else>▼</span>
-								</template>
-								<template v-else>▲▼</template>
-							</button>
-						</div>
-					</th>
-				</tr>
-			</thead>
+			<UsersTableHead
+				:headers="headers"
+				:sort-key="sortKey"
+				:sort-order="sortOrder"
+				:sort-table="sortTable"
+			/>
 			<tbody>
 				<UsersTableRow v-for="row in filteredData" :key="row.id" :row="row" />
 			</tbody>
